516a46db0827491536bb213c85ae2c98aa0e97ab
sifue <yoshimura@soichiro.org>
2022-07-03 18:32:49 +0900

    Projectをdivに組みなおし

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -17,47 +17,44 @@ class Projects extends Component {
     let detailsModalClose = () => this.setState({ detailsModalShow: false });
     if (this.props.resumeProjects && this.props.resumeBasicInfo) {
       var sectionName = this.props.resumeBasicInfo.section_name.projects;
+
+
       var projects = this.props.resumeProjects.map(function (projects) {
-        return (
-          <div className="col-sm-12 col-md-12 col-lg-12" key={projects.title}>
-            <span className="portfolio-item d-block">
-              <div className="foto">
-                <div>
-                  <h1 className="projects-title" style={{ color: "black" }}>
-                    <span>{projects.title}</span>
-                  </h1>
-                  <table className="projects-table">
-                    <thead>
-                      <tr className="projects-table-header">
-                        <th><span>{projects.time_col}</span></th>
-                        <th><span>{projects.title_col}</span></th>
-                        <th><span>{projects.detaillink_col}</span></th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      {projects.content.map((content) => {
-                      return <tr key={content[3]}>
-                        <td className="projects-table-cell">{content[0]}</td>
-                        <td className="projects-table-cell">{content[1]}</td>
-                        <td className="projects-table-cell"><a href={content[3]} target="_blank">{content[2]}</a></td></tr>;})}
-                    </tbody>
-                  </table>
-                </div>
-              </div>
-            </span>
+        return ( <div className="col-md-12">
+          <h1 className="section-title" style={{ color: "black" }} key={projects.title}>
+            <span>{projects.title}</span>
+          </h1>
+          <div className="row">
+              {(projects.content.map((content) => {
+                  return <div
+                      className="col-sm-12 col-md-6 col-lg-4"
+                      key={content[3]}
+                    >
+                      <span className="portfolio-item d-block">
+                        <div className="foto">
+                          <div>
+                            <span className="project-date">{content[0]}</span>
+                            <br />
+                            <p className="projects-title">{content[1]}</p>
+                            <p className="project-title-settings mt-3">
+                              <a href={content[3]} target="_blank">{content[2]}</a>
+                            </p>
+                          </div>
+                        </div>
+                      </span>
+                    </div>;
+                }))}
           </div>
-        );
+
+        </div>);
       });
     }
 
     return (
       <section id="portfolio">
         <div className="col-md-12">
-          <h1 className="section-title" style={{ color: "black" }}>
-            <span>{sectionName}</span>
-          </h1>
           <div className="col-md-12 mx-auto">
-            <div className="row mx-auto">{projects}</div>
+            {projects}
           </div>
         </div>
       </section>
